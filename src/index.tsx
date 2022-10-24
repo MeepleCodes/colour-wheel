f6e95e8cf0512734e1c74604c92c0337ca609f7c
Meeple <awful.meeple@gmail.com>
2022-10-24 18:12:11 +0100

    More warnings, oops

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { ColourWheel } from './ColourWheel';
-import * as m from './ColourModels';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
